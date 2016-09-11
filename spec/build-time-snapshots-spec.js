760e69c3d9b8c6d47bfbf7754dbd35c7101dd223
XLuoChen <1425145741@qq.com>
2016-09-11 13:26:30 +0800

    rename method

--- spec/build-time-snapshots-spec.js
+++ spec/build-time-snapshots-spec.js
@@ -3,11 +3,11 @@ const animals = require('../src/main');
 describe('buildTimeSnapShots', ()=> {
 
   it("build timeSnapshots", ()=> {
-    const snapShots = [`351055db-33e6-4f9b-bfe1-16f1ac446ac1
+    const snapshots = [`351055db-33e6-4f9b-bfe1-16f1ac446ac1
 2016/09/02 22:30:52
 cat1 10 9 2 -1
 cat2 2 3`];
-    const result = animals.buildTimeSnapShots(snapShots);
+    const result = animals.buildTimeSnapshots(snapshots);
     const expectResult = [{
       timeId: '351055db-33e6-4f9b-bfe1-16f1ac446ac1',
       date: '2016/09/02 22:30:52',
